30a23723387471b2c3ff46240135d092c09fd958
Derek Araki-Kurdyla <djak001@bucknell.edu>
2023-11-09 10:01:42 -0500

    Initialize basic classes

---
+++ src/main/java/org/Team6/SorryModel/Board.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI 205 - Software Engineering and Design
+ * Fall 2023
+ * Instructor: Prof. Brian King / Prof. Joshua Stough
+ *
+ * Name: Derek Araki-Kurdyla
+ * Section: 9am-10am
+ * Date: 11/9/23
+ * Time: 10:00 AM
+ *
+ * Project: csci205_final_project
+ * Package: org.Team6.SorryModel
+ * Class: Board
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package org.Team6.SorryModel;
+
+public class Board {
+}
