5654d5ad1521af02165e5d090625e5bf36023da0
Nathan Stamford <nls020@bucknell.edu>
2023-11-13 10:45:16 -0500

    First version of Sorry! Board

--- src/main/java/org/Team6/RoughBoard/RoughBoardFirstTry.java
+++ src/main/java/org/Team6/RoughBoard/RoughBoardFirstTry.java
@@ -7,8 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import javax.swing.plaf.ColorUIResource;
 
 public class RoughBoardFirstTry extends Application {
 
@@ -38,7 +43,7 @@ public class RoughBoardFirstTry extends Application {
         initSceneGraph(root);
 
         // Set up our scene and place the root of the scene graph on it
-        Scene scene = new Scene(root, 700, 600);
+        Scene scene = new Scene(root, 775, 675);
 
         // Set scene on the stage
         primaryStage.setScene(scene);
@@ -63,12 +68,98 @@ public class RoughBoardFirstTry extends Application {
         initRight();
         initLeft();
         initCenter();
+        initRedStartGreenHome();
+        initBlueStartRedHome();
 
         root.setTop(top);
         root.setBottom(bottom);
         root.setRight(right);
         root.setLeft(left);
         root.setCenter(center);
+
+    }
+
+    private void initBlueStartRedHome() {
+        HBox redHomeHBox = new HBox();
+
+        VBox blueStartRedHome = new VBox();
+
+        for (int i = 0; i < 6; i++) {
+            Label spacePadding = new Label();
+            spacePadding.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.EMPTY)));
+            spacePadding.setPrefSize(50, 20);
+            blueStartRedHome.getChildren().add(spacePadding);
+        }
+
+        Circle blueHome = new Circle(42.5, Color.BLUE);
+        blueStartRedHome.getChildren().add(blueHome);
+
+        for (int i = 0; i < 3; i++) {
+            Label filler = new Label();
+            filler.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.EMPTY)));
+            filler.setPrefSize(45,55);
+            blueStartRedHome.getChildren().add(filler);
+        }
+
+        Label redHome = new Label("Home");
+        redHome.setBorder(new Border(new BorderStroke(null,
+                BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+        redHome.setPrefSize(75,75);
+        redHome.setAlignment(Pos.CENTER);
+
+        blueStartRedHome.getChildren().add(redHome);
+
+        for (int i = 0; i < 5; i++) {
+            Label spacePadding = new Label();
+            spacePadding.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            spacePadding.setPrefSize(50, 50);
+            blueStartRedHome.getChildren().add(spacePadding);
+        }
+
+
+        redHomeHBox.getChildren().add(blueStartRedHome);
+        center.setRight(redHomeHBox);
+    }
+
+
+    private void initRedStartGreenHome() {
+
+        VBox greenHomeVBox = new VBox();
+
+        HBox redStartGreenHome = new HBox();
+        for (int i = 0; i < 5; i++) {
+            Label greenSafeZone = new Label();
+            greenSafeZone.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            greenSafeZone.setPrefSize(50,50);
+            redStartGreenHome.getChildren().add(greenSafeZone);
+        }
+
+
+        Label greenHome = new Label("Home");
+        greenHome.setBorder(new Border(new BorderStroke(null,
+                BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+        greenHome.setPrefSize(75,75);
+        greenHome.setAlignment(Pos.CENTER);
+
+        redStartGreenHome.getChildren().add(greenHome);
+        greenHomeVBox.getChildren().add(redStartGreenHome);
+
+        for (int i = 0; i < 3; i++) {
+            Label filler = new Label();
+            filler.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.EMPTY)));
+            filler.setPrefSize(45,45);
+            redStartGreenHome.getChildren().add(filler);
+        }
+
+        Circle redHome = new Circle(42.5, Color.RED);
+        redStartGreenHome.getChildren().add(redHome);
+
+        center.setBottom(greenHomeVBox);
 
     }
 
@@ -103,50 +194,70 @@ public class RoughBoardFirstTry extends Application {
     }
 
     private void initLeft() {
-        left.setPadding(new Insets(15, 12, 15, 12));
         left.setSpacing(0);
         left.setStyle("-fx-background-color: #32CD32;");
 
-        for (int i = 0; i < 16; i++) {
-            Button buttonCurrent = new Button("Space");
-            buttonCurrent.setPrefSize(50, 100);
-            left.getChildren().add(buttonCurrent);
+        for (int i = 0; i < 14; i++) {
+            Label spaceOutline = new Label();
+            spaceOutline.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            spaceOutline.setPrefSize(50,50);
+            left.getChildren().add(spaceOutline);
+
+            //Button buttonCurrent = new Button("Space");
+            //buttonCurrent.setPrefSize(50, 100);
+            //left.getChildren().add(buttonCurrent);
         }
     }
 
     private void initRight() {
-        right.setPadding(new Insets(15, 12, 15, 12));
         right.setSpacing(0);
         right.setStyle("-fx-background-color: #1E90FF;");
 
-        for (int i = 0; i < 16; i++) {
-            Button buttonCurrent = new Button("Space");
-            buttonCurrent.setPrefSize(50, 50);
-            right.getChildren().add(buttonCurrent);
+        for (int i = 0; i < 14; i++) {
+            Label spaceOutline = new Label();
+            spaceOutline.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            spaceOutline.setPrefSize(50,50);
+            right.getChildren().add(spaceOutline);
+
+            //Button buttonCurrent = new Button("Space");
+            //buttonCurrent.setPrefSize(50, 50);
+            //right.getChildren().add(buttonCurrent);
         }
     }
 
     private void initBottom() {
-        bottom.setPadding(new Insets(15, 12, 15, 12));
         bottom.setSpacing(0);
         bottom.setStyle("-fx-background-color: #B22222;");
 
         for (int i = 0; i < 16; i++) {
-            Button buttonCurrent = new Button("Space");
-            buttonCurrent.setPrefSize(50, 50);
-            bottom.getChildren().add(buttonCurrent);
+            Label spaceOutline = new Label();
+            spaceOutline.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            spaceOutline.setPrefSize(50,50);
+            bottom.getChildren().add(spaceOutline);
+
+            //Button buttonCurrent = new Button("Space");
+            //buttonCurrent.setPrefSize(50, 50);
+            //bottom.getChildren().add(buttonCurrent);
         }
     }
 
     private void initTop() {
-        top.setPadding(new Insets(15, 12, 15, 12));
         top.setSpacing(0);
         top.setStyle("-fx-background-color: #FFD700;");
 
         for (int i = 0; i < 16; i++) {
-            Button buttonCurrent = new Button("Space");
-            buttonCurrent.setPrefSize(50, 50);
-            top.getChildren().add(buttonCurrent);
+            Label spaceOutline = new Label();
+            spaceOutline.setBorder(new Border(new BorderStroke(null,
+                    BorderStrokeStyle.SOLID, new CornerRadii(4), BorderWidths.DEFAULT)));
+            spaceOutline.setPrefSize(50,50);
+            top.getChildren().add(spaceOutline);
+
+            //Button buttonCurrent = new Button("Space");
+            //buttonCurrent.setPrefSize(50, 50);
+            //top.getChildren().add(buttonCurrent);
         }
 
         // Button buttonCurrent = new Button("Current");
