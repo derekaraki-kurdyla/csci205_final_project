95b06a251423f4661a20d021fc1123dadd95f907
Alex Araki-Kurdyla <amak001@bucknell.edu>
2023-11-16 10:04:33 -0500

    Finished game rules file and added to board class

--- src/main/java/org/Team6/SorryModel/Board.java
+++ src/main/java/org/Team6/SorryModel/Board.java
@@ -50,13 +50,13 @@ public class Board {
 
     public ArrayList<Pawn> greenStart = new ArrayList<>();
 
-    public ArrayList<Space> redHome = new ArrayList<>();
+    public ArrayList<Spaces> redHome = new ArrayList<>();
 
-    public ArrayList<Space> blueHome = new ArrayList<>();
+    public ArrayList<Spaces> blueHome = new ArrayList<>();
 
-    public ArrayList<Space> yellowHome = new ArrayList<>();
+    public ArrayList<Spaces> yellowHome = new ArrayList<>();
 
-    public ArrayList<Space> greenHome = new ArrayList<>();
+    public ArrayList<Spaces> greenHome = new ArrayList<>();
 
 
 
